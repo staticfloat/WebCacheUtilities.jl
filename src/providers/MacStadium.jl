499bcf25cd2409849d567c6a9ca233489543cf98
Elliot Saba <staticfloat@gmail.com>
2020-06-08 12:47:05 -0700

    Expand MacStadium IPs

--- src/providers/MacStadium.jl
+++ src/providers/MacStadium.jl
@@ -1,6 +1,10 @@
 module MacStadium
 import ..IPSubnet, ..extract_subnets_from_ipinfo
 
-prefixes() = extract_subnets_from_ipinfo("AS395336")
+prefixes() = vcat(
+    extract_subnets_from_ipinfo("AS395336"),
+    extract_subnets_from_ipinfo("AS54112"),
+    extract_subnets_from_ipinfo("AS397114"),
+)
 
 end # module MacStadium
